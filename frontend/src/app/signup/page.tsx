63552f85c8bf11c7f29a05e33d1d1fa23f9d73db
Renato Filogenio <Renato.filogenio@gmail.com>
2025-07-14 15:46:14 -0300

    ajustando variaveis da vercel

--- frontend/src/app/signup/page.tsx
+++ frontend/src/app/signup/page.tsx
@@ -1,53 +1,51 @@
 import { api } from "../services/api";
 import style from "./page.module.scss"
 import Link from "next/link";
+
 export default function Signup(){
 
-    async function teste(formData:FormData){
-      "use server"
-      
-      try {
-        
-          const email = formData.get("email")
-          const name = formData.get("name")
-          const password = formData.get("password")
+  async function teste(formData:FormData){
+    "use server"
+    
+    try {
+      const email = formData.get("email");
+      const name = formData.get("name");
+      const password = formData.get("password");
 
-        const userCreated = await api.post("/user", {
-          name: name,
-          email: email,
-          password: password
-        })
+      await api.post("/user", {
+        name,
+        email,
+        password
+      });
       
-        
-      } catch (error) {
-        
-      }
-       
+    } catch{
+      
     }
+     
+  }
 
   return(
     <div className={style.mainDivSignup}> 
       <br />
-            <h1 className={style.logH1}>Cadastra-se</h1>
+      <h1 className={style.logH1}>Cadastra-se</h1>
 
-            <p>Apenas gmail é aceito</p>
-            <p>A senha deve conter no minimo 8 caracteres</p>
+      <p>Apenas gmail é aceito</p>
+      <p>A senha deve conter no minimo 8 caracteres</p>
+
       <form action={teste} className={style.FormSignup}>
 
-      <input type="email" placeholder="Email" name="email"/>
-      <br />
-      <input type="text"  placeholder="Nome" name="name"/>
-      <br />
-      <input type="password" placeholder="Senha"  name="password"/>
-      <br />
-      <button type="submit">Cadastrar-se</button>
+        <input type="email" placeholder="Email" name="email"/>
+        <br />
+        <input type="text"  placeholder="Nome" name="name"/>
+        <br />
+        <input type="password" placeholder="Senha"  name="password"/>
+        <br />
+        <button type="submit">Cadastrar-se</button>
 
         <Link href={"/"} className={style.signupLink}>
-              <button>Ir para login</button>
-            </Link>
+          <button>Ir para login</button>
+        </Link>
       </form>
-
-           
     </div>
   );
 }
